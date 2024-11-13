72aa8ba985e344353c9782bc4208492baef25e64
Bartomeu Mut Vidal <tomeu95@hotmail.com>
2024-11-13 17:56:04 +0100

    Add Page Index and components Todo

---
+++ components/Todo/Form.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form class="flex" @submit.prevent="addTodo">
+      <input
+        v-model="todoName"
+        class="c-input__text"
+        placeholder="Ex: Practise my English speaking people"
+      />
+      <button class="c-btn c-btn__input" type="submit">
+        Add
+        <i class="fas fa-plus"></i>
+      </button>
+    </form>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      todoName: '' as string,
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.todoName) {
+        this.$store.commit('todos/add', this.todoName)
+        this.todoName = ''
+      } else {
+        alert('Please fill the Task Field!')
+      }
+    },
+  },
+}
+</script>
---
+++ components/Todo/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="show" class="c-modal__bg">
+    <div class="c-modal__content">
+      <div class="c-modal__title">Are you sure to delete this item?</div>
+      <div class="c-modal__todo">{{ todo.name }}</div>
+      <div class="flex items-center gap-4 p-8 justify-center">
+        <button class="c-btn__delete" @click="$emit('confirm')">
+          Delete
+          <i class="fas fa-times"></i>
+        </button>
+        <button class="c-btn__cancel" @click="$emit('cancel')">
+          Cancel
+          <i class="fas fa-times-circle"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+    todo: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
